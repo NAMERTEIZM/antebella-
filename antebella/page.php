91f4640a0a6a260d7c0d412e02657572081d06d4
Mert Musabeseoglu <109963581+NAMERTEIZM@users.noreply.github.com>
2022-08-02 17:32:58 +0300

    Add files via upload

---
+++ antebella/page.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+   <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+ 
+   <title>antebella</title>
+</head>
+<body>
+    <div class="container" id="first-container">
+<div class="row">
+<div class="col-xl-3" id="logokapsar" >
+<img src="img/Group 71@3x.png" id="logo">
+</div>   
+<div class="col-xl-7 " id="navbar">
+<ul>
+
+  <span id="icerik">
+  <li><a href="#news">ürünler</a></li>
+  <li><a href="#contact">lezzetin hikayesi</a></li>
+  <li><a href="#about">antepsan</a></li>
+  <li><a href="#about">bize ulaşın</a></li>
+  </span>
+</ul>
+</div>
+
+<div class="col-xl-2" id="social">
+
+<img src="img/yt.png" alt="#">
+<span id="nasılyani"><img src="img/fb.png" alt="#"></span>
+<img src="img/tw.png" alt="#">
+<img src="img/ig.png" alt="#">
+
+</div>
+
+</div>
+<div class="container" id="cont">
+<div class="row">
+    <div class="col-xl-3" id="aa">
+<a>Antepsan<br>
+hakkında</a>
+    </div>
+
+   </div>
+<div class="row">
+    <div class="col-xl-9" id="aac">
+    Antepli bir çiftçi, yenilikçi bakış açısı ve fıstığa duyduğu aşkla yıllarca fıstık ticareti yaptı ve yüzlerce üreticiden aldığı fıstığı inceleyerek en mükemmel fıstığı aradı. 2008 yılında yatırımlarını hız kazandırarak Türkiye’nin en kaliteli antep fıstığı üreticisi olma yolunda önemli adımlar attı. Birkaç yıl sonra, kalite ve inovasyon odaklı atılan adımların karşılığı, Antepsan firmasını Türkiye’nin en büyük zincir marketlerinin; en büyük üreticisi konumuna taşıdı. Kendi bölgesinin insanını, çiftçisini ve ağaçlarını korumak, büyütmek ve kalkındırmak şiarıyla yapılan yepyeni yatırımlarla Antepsan, 2008’den bu yana %550 büyüme göstererek ülkemizin ve Antep sanayisinin göz bebeği konumuna geldi.
+
+<p></p>Antepsan kendini yüksek kaliteli, sağlıklı ve en doğal ürünler sunmaya, tüketicilerin her gün daha iyi seçimler yapmalarına yardımcı olmaya adamış özel bir şirkettir. Kuruyemişler, sürülebilir ürünler, baklava ve pestil ürünleri dahil olmak üzere birçok çeşitte ürünü en kaliteli şekilde sunuyoruz. Antepsan’ın giderek büyüyen pazar ağı ve tedarik zinciri, dünyanın ve Türkiye’nin en büyük ağaç yemişleri yetiştiricisi, narenciye yetiştiricisi ve sürülebilir fıstık ezmesi grubunda öncü bir firma olma konumuna taşıyor.
+
+<p></p>Antepsan ailesi olarak her zaman sağlıklı, kaliteli ve yenilikçi ürünlerle ilham veren bir marka olmaya, ülke ekonomimizi ve çiftçimizi en ileriye taşımaya devam ediyoruz.
+
+    </div>
+</div>
+</div>
+    </div>
+
+    <div class="container" id="second-container">
+        
+    <div class="row">
+       
+    <div class="col-xl-8" id="fl">
+    <div class="row">
+    <img src="img/Group 71@3x.png" id="logo2">  
+    </div>
+   
+    <div class="row">
+<div id="yazilar">
+<h1>İletişim</h1>
+<p>Başpınar O.S.B. 5. Bölge 83524 nolu cad. no :14 Şehitkamil/GAZİANTEP /TURKEY<br>
+İstanbul Ofis : Nish İstanbul Sanayi Caddesi No:44 B Daire: 42 Çobançeşme / Bahçelievler / İstanbul 
+</p>
+<span>iletişim@antebella.com.tr</span>
+</div>
+    </div>
+    
+    <div class="row">
+<div id="aciklama">
+<span>Antebella bir Antepsan Markasıdır</span>
+</div>
+    </div> 
+    
+</div>
+
+
+       <div class="col-xl-4" id="fr">
+
+      <div class="row">
+<h6>Bizi Takip Edin</h6>
+      </div>
+      <div class="row">
+<div id="ftimg">
+<img src="img/ig-s.png" alt="#">
+<img src="img/tw-s.png" alt="#">
+<img src="img/yt-s.png" alt="#">
+<img src="img/ffb-s.png" alt="#">
+</div>
+        </div>
+        <div class="row">
+        <span id="ftacik">
+        Antebella hakkında güncel haberleri<br>
+        Almak ve fırsatları kaçırmamak için<br>
+        Bültenimize kayıt olun
+        </span>
+        </div>
+        <div class="row">
+        <button id="buton" type="text">Kayıt Ol</button>
+        </div>
+        <div class="row">
+        
+        <img id="antepsan" src="img/antepsan.png" alt="">
+        </div>
+        
+</div>
+
+
+        </div>
+   
+
+</div>
+<div class="third-container">
+    <div class="row">
+    
+    <div class="col-xl-12">
+        <div class="row">
+
+    <div class="col-xl-6" id="fbl">
+ <span>© Copyright 2021,  Designed by Ket Kolektif</span>
+    </div>
+    <div class="col-xl-6" id="fbr">
+        <span>İçerik llanım Şartları   |    KVKK Metni</span>
+        </div>
+
+        
+    </div>
+
+    </div>
+</div>
+
+</body>
+</html>
